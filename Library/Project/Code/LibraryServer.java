8d0d93abced9288ae8f52d848e7d5b6b7a456094
RoxasTA <victorbr64g@gmail.com>
2024-06-16 00:15:46 -0300

    RRRR

--- Library/Project/Code/LibraryServer.java
+++ Library/Project/Code/LibraryServer.java
@@ -1,5 +1,23 @@
-class Scratch {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class LiberyServer {
+    private static final  String JSONFIFILE = "";
+    private static  List<BOOK> books;
+
     public static void main(String[] args) {
-        
+
+        final int PORTA = 1337;
+
+
     }
 }
